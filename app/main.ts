0c331f10ad9149564564194725eba8319afdf511
기민진 <minjin.ki@campmobile.com>
2017-01-18 10:38:47 +0900

    aot 컴파일

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
 
-platformBrowserDynamic().bootstrapModuleFactory(AppModuleNgFactory);
+/*
+ * AOT Compile
+ */
+//platformBrowserDynamic().bootstrapModuleFactory(AppModuleNgFactory);
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
